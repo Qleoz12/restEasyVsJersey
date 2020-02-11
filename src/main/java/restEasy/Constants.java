fe371874e804cdc67d2b21079ac343d352282e78
Dann <qleoz12.leonardo@gmail.com>
2020-02-10 19:32:52 -0500

    first commit

---
+++ src/main/java/restEasy/Constants.java
@@ -0,0 +1,14 @@
+package util;
+
+public final class Constants {
+
+    private Constants() {
+            // restrict instantiation
+    }
+
+    public static final String url = "http://10.1.0.201:8086/pines-rest/rest/pyz/";
+    public static final String url_test = "http://192.168.6.89:8086/pines-rest/rest/pyz/";
+    public static final String url_test2 = "http://192.168.6.89:8086/pines-rest/rest/test/";
+    public static final String BearerToken = "f3d76b4f-4d66-3e6c-9329-936b725eb99c";
+    
+}
---
+++ src/main/java/restEasy/client.java
@@ -0,0 +1,57 @@
+package services;
+
+import javax.ws.rs.client.WebTarget;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+public enum client {
+	INSTANCE;
+	private ResteasyClient clientweb;
+	private WebTarget target;
+	private String url;
+
+	private client() {
+		System.out.println("---------- i am called!");
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JSR310Module());
+		JacksonJsonProvider provider = new JacksonJsonProvider(mapper);
+		clientweb = new ResteasyClientBuilder()
+						.register(provider)
+						.build();
+	}
+
+	
+	public WebTarget getTarget() {
+		return target;
+	}
+
+	public void setTarget(WebTarget target) {
+		this.target = target;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+	public ResteasyClient getClientweb() {
+		return clientweb;
+	}
+
+
+	public void setClientweb(ResteasyClient clientweb) {
+		this.clientweb = clientweb;
+	}
+	
+	
+
+}
